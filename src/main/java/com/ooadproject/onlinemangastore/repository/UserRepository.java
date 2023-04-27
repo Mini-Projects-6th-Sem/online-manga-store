6ee2937afadc0cfdc38418d772e8985b65e4178b
Adeeshjn <92499185+Adeeshjn@users.noreply.github.com>
2023-04-27 14:11:55 +0530

    Delete src directory

--- src/main/java/com/ooadproject/onlinemangastore/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ooadproject.onlinemangastore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.ooadproject.onlinemangastore.model.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
